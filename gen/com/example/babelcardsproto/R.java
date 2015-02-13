073a9833fc27d56b1433c2c8a5606a4d1a88a485
maudnals <maud.nalpas@gmail.com>
2015-02-13 07:50:07 +0100

    Feat(review): dynamic fragments = f(review mode)

--- gen/com/example/babelcardsproto/R.java
+++ gen/com/example/babelcardsproto/R.java
@@ -140,33 +140,37 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int addButton=0x7f08000d;
-        public static final int checkBoxScore=0x7f080004;
-        public static final int grid_text=0x7f08000c;
-        public static final int gridview=0x7f080002;
+        public static final int action_settings=0x7f080012;
+        public static final int addButton=0x7f080003;
+        public static final int checkBoxScore=0x7f080007;
+        public static final int fragmentContainer=0x7f08000d;
+        public static final int grid_text=0x7f080002;
+        public static final int gridview=0x7f080005;
         public static final int pager=0x7f080000;
-        public static final int picker=0x7f08000a;
-        public static final int playButton=0x7f080009;
-        public static final int radioButtonCustom=0x7f080007;
-        public static final int radioButtonRandom=0x7f080008;
-        public static final int radioButtonSmart=0x7f080006;
-        public static final int review_row_title=0x7f08000e;
-        public static final int review_switch=0x7f08000f;
+        public static final int picker=0x7f080004;
+        public static final int playButton=0x7f08000e;
+        public static final int radioButtonCustom=0x7f08000b;
+        public static final int radioButtonRandom=0x7f08000c;
+        public static final int radioButtonSmart=0x7f08000a;
+        public static final int radioGroupReviewMode=0x7f080009;
+        public static final int review_row_title=0x7f08000f;
+        public static final int review_switch=0x7f080010;
         public static final int section_label=0x7f080001;
-        public static final int section_label_stats=0x7f08000b;
-        public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080005;
+        public static final int section_label_stats=0x7f080011;
+        public static final int textViewReviewModeOption=0x7f080008;
+        public static final int textViewScoreOption=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
-        public static final int fragment_tab_create=0x7f030002;
-        public static final int fragment_tab_review=0x7f030003;
-        public static final int fragment_tab_stats=0x7f030004;
-        public static final int grid_item=0x7f030005;
-        public static final int grid_item_add=0x7f030006;
-        public static final int tab_review_row=0x7f030007;
+        public static final int grid_item=0x7f030002;
+        public static final int grid_item_add=0x7f030003;
+        public static final int review_custom=0x7f030004;
+        public static final int review_random=0x7f030005;
+        public static final int tab_create=0x7f030006;
+        public static final int tab_review=0x7f030007;
+        public static final int tab_review_row=0x7f030008;
+        public static final int tab_stats=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f070000;
